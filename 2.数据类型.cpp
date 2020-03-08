fe86ce3dc2bcedf53cbaf353a8a3d291f8a6af24
张正 <zhangzheng@email.com>
2020-03-08 17:26:41 +0800

    数据类型
    Signed-off-by: 张正 <zhangzheng@email.com>

---
+++ 2.数据类型.cpp
@@ -0,0 +1,27 @@
+/**
+ * 2、数据类型
+ *
+ * 	基本数据类型
+ * 		整形
+ * 			整形		int			4 字节
+ * 			短整形		short int	2
+ * 			长整形		ling int	8
+ * 			
+ * 		浮点数
+ * 			单精度		float		4
+ * 			双精度		double		8
+ * 			
+ * 		字符
+ * 			字符		char		1
+ * 		
+ *   复合类型数据
+ */
+
+# include <stdio.h>
+
+int main()
+{
+	int i = 10;
+
+	printf("%d\n",  i);
+}
