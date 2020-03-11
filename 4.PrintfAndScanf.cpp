1cdb311af1562f41349f8a5dde92cc4b4ef28253
张正 <zhangzheng@email.com>
2020-03-11 14:28:14 +0800

    输入输出与运算符

---
+++ 4.PrintfAndScanf.cpp
@@ -0,0 +1,31 @@
+/**
+ * 4、PrintfAndScanf
+ * 
+ * printf 	输出
+ * scanf 	输入
+ *
+ * 控制符
+ * %d		整数十进制
+ * %o		整数八进制
+ * %x		整数十六进制(小写)
+ * %X		整数十六进制(大写)
+ * %f		单精度
+ * %lf		双精度
+ * %c		字符
+ * %s 		字符串
+ */
+
+# include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("请输入一个数字：");
+
+    scanf("%d",&i);
+
+    printf("你输入的数字是：%d。\n",i);
+
+    return 0;
+}
