a30bb5911817507385fcae5849f91b89a0b55816
张正 <zhangzheng@email.com>
2020-03-12 10:09:27 +0800

    for循环99乘法标

--- 7.循环语句.cpp
+++ 7.循环语句.cpp
@@ -18,12 +18,25 @@
 
 int main()
 {
-	for (int i = 0; i <= 100; ++i)
+	for (int i = 1; i <= 100; ++i)
 	{
 		printf("I = %d\n", i);
 	}
 
 	printf("For循环结束！\n");
 
+	// 循环嵌套,99乘法表
+	for (int i = 1; i <= 9; ++i) {
+
+	    for (int j = 1; j <= i; ++j) {
+
+	        printf("%d * %d = %d \t",i,j,(i*j));
+	        
+	    }
+
+	    printf("\n");
+	    
+	}
+	
 	return 0;
 }
