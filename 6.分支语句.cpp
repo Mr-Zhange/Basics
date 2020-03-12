68de5110e56ace9dfe38ed1e84a349bdc218c0c4
张正 <zhangzheng@email.com>
2020-03-12 14:08:15 +0800

    switch分支语句

--- 6.分支语句.cpp
+++ 6.分支语句.cpp
@@ -10,6 +10,18 @@
  * 		{
  * 			代码段2
  * 		}
+ *
+ * 	switch 语句，格式
+ * 		seutch(i)	//i可以是数字或字符
+ * 		{
+ * 			case 1:
+ * 				代码段
+ * 				break;		//	停止执行的意思，跳出循环、分支
+ * 			case 2:
+ * 				代码段
+ * 			default:
+ * 				代码段
+ * 		}
  * 		
  */
 
@@ -33,6 +45,30 @@ int main() {
     else
     {
         printf("你的成绩是：C\n");
+    }
+
+    int i = 1;
+    char ch = '1';
+
+    switch (ch)
+    {
+        case '1':
+            printf("这是1");
+            break;
+        case '2':
+            printf("这是2");
+            break;
+        case '3':
+            printf("这是3");
+            break;
+        case '4':
+            printf("这是4");
+            break;
+        case '5':
+            printf("这是5");
+            break;
+        default:
+            printf("这是其他值");
     }
 
     return 0;
